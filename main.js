8d10e530110ec68974ce6d9c9b1d32cf1f244db3
Ard-Jan van Etten <arte@redstar.fritz.box>
2020-05-20 22:00:15 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,86 @@
+let canvas = document.getElementById("canvas")
+let ctx = canvas.getContext("2d")
+let width = canvas.width
+let height = canvas.height
+
+let gs = 50
+let gaps = 20
+let transparency = 0.6
+let blur = 0
+
+let t = 8
+let v = 13
+let f = 21
+
+let x = 900
+let y = 1300
+
+let i = 2
+
+//ctx.beginPath()
+
+var img = new Image()
+img.onload = main
+img.src = 'https://cdn.discordapp.com/attachments/572855721520005146/709780363219566732/arctic-sunset.jpg'
+
+function main(){
+	ctx.drawImage(img, 0,0, canvas.width, canvas.height)
+
+	
+
+	while(f < width){
+
+		f = v + t
+
+		switch (i % 4) {
+
+			case 0:
+				x -= f
+			break
+
+			case 1:
+				y += v
+			break
+
+			case 2:
+				x += v
+				y -= t
+			break
+
+			case 3:
+				x -= t
+				y -= f
+			break
+
+		}
+
+		t = v
+		v = f
+
+		i++
+
+		ctx.fillStyle = `rgba(${gs}, ${gs}, ${gs}, ${transparency})`
+		ctx.filter = `blur(${blur}px)`
+		ctx.shadowBlur = blur
+		ctx.shadowOffsetX = 0
+		ctx.shadowOffsetY = 0
+		ctx.shadowColor = `rgb(${gs}, ${gs}, ${gs}`
+
+		ctx.fillRect(x + gaps, y + gaps, f - gaps, f - gaps)
+
+		ctx.fillStyle = 'rgba(255,255,255,1)'
+		ctx.filter = 'none'
+		ctx.font = `${t/3}px monospace`
+		ctx.textAlign = 'center'
+		ctx.fillText(t, x + f / 2, y + f / 2 + t / 6)
+
+	/*
+		let g = i % 4
+
+		console.log({g,x,y,f,v,t})*/
+
+	//ctx.stroke()
+
+	}
+}
+
